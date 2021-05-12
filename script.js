975e5ecf3ffcc9c7e93900f954f6dcdda5999db5
Jendayi Retana <jendayi.retana@meritknights.com>
2021-05-12 00:02:11 -0600

    working audio, still missing some notes

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ keydown is the event type
 event is the event
 and if statement checks the key
 */
+
 // credit for keyboard function: https://javascript.info/keyboard-events
-// list credit: https://stackoverflow.com/questions/30289217/how-do-i-loop-through-a-javascript-array-of-audio-files and https://code-boxx.com/play-audio-javascript/
+// list or array credit: https://stackoverflow.com/questions/30289217/how-do-i-loop-through-a-javascript-array-of-audio-files and https://code-boxx.com/play-audio-javascript/
 // audio credit: https://freesound.org/people/pinkyfinger/
 
 var audio = new Audio();
@@ -27,39 +28,63 @@ var notes = [
 window.addEventListener("keydown", function(event){
   if (event.code === "KeyA") {
     text = "C";
+    audio = new Audio(notes[3]);
+    audio.play();
   } if (event.code === "KeyS") {
     text = "D";
+    audio = new Audio(notes[5]);
+    audio.play();
   } if (event.code === "KeyD") {
     text = "E";
+    audio = new Audio(notes[7]);
+    audio.play();
   } if (event.code === "KeyF") {
     text = "F";
+    audio = new Audio(notes[8]);
+    audio.play();
   } if (event.code === "KeyG") {
     text = "G";
+    audio = new Audio(notes[10]);
+    audio.play();
   } if (event.code === "KeyH") {
     text = "A";
+    audio = new Audio(notes[0]);
+    audio.play();
   } if (event.code === "KeyJ") {
     text = "B";
-  } if (event.code === "KeyK") {
+    audio = new Audio(notes[2]);
+    audio.play();
+  } /*if (event.code === "KeyK") {
     text = "C";
   } if (event.code === "KeyL") {
     text = "D";
   } if (event.code === "Semicolon") {
     text = "E";
-  } if (event.code === "KeyW") {
+  }*/ if (event.code === "KeyW") {
     text = "C#";
+    audio = new Audio(notes[4]);
+    audio.play();
   } if (event.code === "KeyE") {
     text = "D#";
+    audio = new Audio(notes[6]);
+    audio.play();
   } if (event.code === "KeyT") {
     text = "F#";
+    audio = new Audio(notes[9]);
+    audio.play();
   } if (event.code === "KeyY") {
     text = "G#";
+    audio = new Audio(notes[11]);
+    audio.play();
   } if (event.code === "KeyU") {
     text = "A#";
-  } if (event.code === "KeyO") {
+    audio = new Audio(notes[1]);
+    audio.play();
+  } /*if (event.code === "KeyO") {
     text = "C#";
   } if (event.code === "KeyP") {
     text = "D#";
-  }
+  }*/
   document.getElementById("demo").innerHTML = text;
 })
 
